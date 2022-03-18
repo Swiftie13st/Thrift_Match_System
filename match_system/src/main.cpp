752e903bce01f265e218a754b6b0a79027731c89
Bruce Shao <swiftie13s@163.com>
2022-03-18 14:22:47 +0800

    Match System 3.0 : When dt(score) <= 50 Match

--- match_system/src/main.cpp
+++ match_system/src/main.cpp
@@ -16,6 +16,7 @@
 #include <condition_variable>
 #include <queue>
 #include <vector>
+#include <unistd.h>
 
 using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
@@ -48,11 +49,24 @@ class Pool
         {
             while(users.size() > 1)
             {
-                auto a = users[0], b = users[1];
-                users.erase(users.begin());
-                users.erase(users.begin());
+                //按分数排序
+                sort(users.begin(), users.end(), [&](User &a, User b){
+                    return a.score < b.score;
+                        });
 
-                save_result(a.id, b.id);
+                bool flag = true; //防止死循环，当没有匹配上时跳出循环
+                for(uint32_t i = 1; i < users.size(); i ++)
+                {
+                    auto a = users[i - 1], b = users[i];
+                    if(b.score - a.score <= 50)
+                    {
+                        users.erase(users.begin() + i - 1, users.begin() + i + 1);
+                        save_result(a.id, b.id);
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag) { break; }
             }
 
         }
@@ -161,7 +175,10 @@ void consume_task()
         unique_lock<mutex> lck(message_queue.m);
         if(message_queue.q.empty())
         {
-            message_queue.cv.wait(lck);
+            // message_queue.cv.wait(lck);
+            lck.unlock();
+            pool.match();
+            sleep(1);   // 每秒匹配一次
         }
         else
         {
@@ -177,7 +194,7 @@ void consume_task()
                 pool.remove(task.user);
             }
 
-            pool.match();
+       
 
         }
     }
