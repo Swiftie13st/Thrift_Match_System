cf1e35353fe1b23e844fe2ad54e148d432d46d14
Bruce Shao <swiftie13s@163.com>
2022-03-18 11:33:31 +0800

    Match System 2.0 : Add Save_result

--- match_system/src/main.cpp
+++ match_system/src/main.cpp
@@ -2,10 +2,13 @@
 // You should copy it to another filename to avoid overwriting it.
 
 #include "match_server/Match.h"
+#include "save_client/Save.h"
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/server/TSimpleServer.h>
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TBufferTransports.h>
+#include <thrift/transport/TSocket.h>
+#include <thrift/transport/TTransportUtils.h>
 
 #include <iostream>
 #include <thread>
@@ -20,6 +23,8 @@ using namespace ::apache::thrift::transport;
 using namespace ::apache::thrift::server;
 
 using namespace ::match_service;
+using namespace ::save_service;
+
 using namespace std;
 
 struct Task
@@ -55,6 +60,29 @@ class Pool
         void save_result(int a, int b)
         {
             cout << "Match Result: " << a << ' ' << b << endl;
+            std::shared_ptr<TTransport> socket(new TSocket("123.57.47.211", 9090));
+            std::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+            std::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+            SaveClient client(protocol);
+
+            try {
+                transport->open();
+
+                int res = client.save_data("acs_3447", "eb4853ef", a, b);
+                if(res == 0)
+                {
+                    cout << "Success" << endl;
+                }
+                else if(res == 1)
+                {
+                    cout << "Error" << endl;
+                }
+
+
+                transport->close();
+            } catch (TException& tx) {
+                cout << "ERROR: " << tx.what() << endl;
+            }
         }
 
         void add(User user)
